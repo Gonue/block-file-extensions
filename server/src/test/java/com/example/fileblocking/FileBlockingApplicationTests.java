44241c939ca83b0df99bb9a3662431e591b1a759
Gonue <6017891@naver.com>
2023-07-28 11:50:31 +0900

    init : server init

---
+++ server/src/test/java/com/example/fileblocking/FileBlockingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileblocking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileBlockingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
